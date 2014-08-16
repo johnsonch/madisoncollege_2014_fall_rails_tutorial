825a8def6cc46541847ddcba4948923e28ec7974
johnsonch <chris@johnsonch.com>
2014-08-16 01:02:53 +0000

    end of chapter 5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,54 +1,54 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  subject { page }
+
   describe "Home page" do
-    it 'has the content "Sample App"' do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
-    
-    it "has a title of Home" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
-    end
-    
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Home')
-    end
+
+    before { visit home_path }
+
+    it{ should have_content('Sample App') }
+    it{ should have_title("Ruby on Rails Tutorial Sample App") }
+    it{ should_not have_title('| Home') }
   end
+
   describe "Help page" do
+
+    before { visit help_path }
+
     it 'has the content "Help"' do
-      visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
     
     it "has a title of Help" do
-      visit '/static_pages/help'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
   
    describe "About page" do
+
+    before { visit about_path }
+
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
      
      it "has a title of About" do
-       visit '/static_pages/about'
        expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
      end
   end
   
   describe "Contact page" do
+
+    before { visit contact_path }
+
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
       expect(page).to have_content('Contact')
     end
     it "has a title of Contact" do
-      visit '/static_pages/contact'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
     end
   end
+
 end
